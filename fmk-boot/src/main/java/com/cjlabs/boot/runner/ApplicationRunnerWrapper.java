a97219ea3ed55bceea12f74987760f60e7f298ae
jamie20241210 <jamie20241210@gmail.com>
2025-10-25 10:34:30 +0700

    feat: FmkService

---
+++ fmk-boot/src/main/java/com/cjlabs/boot/runner/ApplicationRunnerWrapper.java
@@ -0,0 +1,28 @@
+package com.cjlabs.boot.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@Slf4j
+public class ApplicationRunnerWrapper {
+
+    /**
+     * 启动 Spring Boot 应用，捕获异常并处理
+     *
+     * @param applicationClass 启动类
+     * @param args             启动参数
+     * @return Spring 上下文
+     */
+    public static ConfigurableApplicationContext run(Class<?> applicationClass, String[] args) {
+        try {
+            log.info("ApplicationRunnerWrapper|run|name={}", applicationClass.getSimpleName());
+            return SpringApplication.run(applicationClass, args);
+        } catch (Exception e) {
+            log.info("ApplicationRunnerWrapper|run|failed={}", e.getMessage(), e);
+            // TODO: 可以在这里做补救操作，如通知、释放资源、记录状态等
+            throw e;
+            // 可选择继续抛出异常或终止应用
+        }
+    }
+}
---
+++ fmk-boot/src/test/java/com/cjlabs/boot/aaa.java
@@ -0,0 +1,4 @@
+package com.cjlabs.boot;
+
+public class aaa {
+}
