7a89f1cdc95dbdfc7a057d13dd3964a48457b263
jamie <jamie20241210@gmail.com>
2025-11-27 11:40:44 +0700

    feat: kex fmk

--- fmk-core/src/test/java/com/cjlabs/core/time/FmkInstantUtilTest.java
+++ fmk-core/src/test/java/com/cjlabs/core/time/FmkInstantUtilTest.java
@@ -21,7 +21,7 @@ public class FmkInstantUtilTest {
         String instant2Str = FmkInstantUtil.formatDateTime(instant2);
         log.info("FmkInstantUtilTest|main|instant2Str={}", instant2Str);
 
-        Instant dbInstant1 = FmkInstantUtil.fromMillis(1764181951000L);
+        Instant dbInstant1 = FmkInstantUtil.fromMillis(1764176400000L);
         String dbInstant1Str = FmkInstantUtil.format(dbInstant1, yy_MM_dd_HH_mm_ss_Formatter, SINGAPORE_ZONE);
         log.info("FmkInstantUtilTest|main|dbInstant1Str={}", dbInstant1Str);
     }
